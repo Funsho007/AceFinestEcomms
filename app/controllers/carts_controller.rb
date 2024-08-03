347292b8eed7a123440744d686105bc2af22f0e4
Funsho007 <yinkaoke007@gmail.com>
2024-08-03 10:28:23 -0500

    cart controller updated

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,8 @@ class CartsController < BaseController
   add_breadcrumb "home", :root_path
   def show
     add_breadcrumb "products", products_path
-    @order = current_order
-    @order_items = current_order&.order_items
+    @order_items = current_order.order_items
+  rescue ActiveRecord::RecordNotFound
+    @order_items = []
   end
 end
