6489fa0070117e0ccb6cbad47e9289c5963f1479
Funsho007 <yinkaoke007@gmail.com>
2024-08-03 10:44:02 -0500

    cart controller edited

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,17 @@
 class CartsController < BaseController
-
- 
-
   add_breadcrumb "home", :root_path
+
   def show
     add_breadcrumb "products", products_path
-    def show
-      add_breadcrumb "products", products_path
-      @order_items = current_order.try(:order_items)
-    rescue ActiveRecord::RecordNotFound
-      @order_items = []
-    end
+    @order_items = current_order.try(:order_items) || []
+  rescue ActiveRecord::RecordNotFound
+    @order_items = []
+  end
+
+  private
+
+  def current_order
+    @current_order ||= Order.find(session[:order_id]) if session[:order_id]
+    @current_order ||= Order.create(user: current_user)
   end
 end
